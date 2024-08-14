645cac458967646bf9f2adcaa2868b6146dd462d
Quinn Qiu <quinnqiu0823@icloud.com>
2024-08-14 19:48:33 +0800

    Update page.js

--- scripts/page.js
+++ scripts/page.js
@@ -1,31 +1,28 @@
 const pages = {
     "Blogs": "blogs.html",
     "Projects": "projects.html",
-    "Standard": "standard.html",
+    "Standards": "standards.html",
     "Music": "music.html",
     "Website": "website.html"
 };
 const footer_directory_items = {
-    0: {},
-    1: {
+    0: {
         "Blogs": {
-            "Top Blogs": "blogs.html",
             "Tags": "blogs.html",
             "Archives": "blogs.html"
         }
     },
-    2: {
+    1: {
         "Projects": {
-            "Top Projects": "projects.html",
             "Web Projects": "projects.html",
             "Scratch Projects": "projects.html"
         },
         "Standards": {
-            "Active": "standard.html",
-            "Voided": "standard.html",
+            "Active": "standards.html",
+            "Voided": "standards.html",
         }
     },
-    3: {
+    2: {
         "Developer": {
             "GitHub": "https://github.com/Quinn0823/",
             "Gitter": "https://matrix.to/#/@quinn0823-6126f7736da0373984840004:gitter.im",
@@ -39,16 +36,19 @@ const footer_directory_items = {
             "知乎": "https://www.zhihu.com/people/quinn0823"
         }
     },
-    4: {
+    3: {
         "Quinn-Web": {
             "Repository": "https://github.com/Quinn0823/web_preview",
             "Releases": "https://github.com/Quinn0823/web_preview/releases",
             "Commits": "https://github.com/Quinn0823/web_preview/commits/main",
-            "Issues": "https://github.com/Quinn0823/web_preview/issues",
-            "Website": "website.html"
+            "Issues": "https://github.com/Quinn0823/web_preview/issues"
         }
     }
 }
+const footer_note_items = [
+    "Quinn-Web Preview is under development. Go to <a href='https://quinn0823.github.io' target='_blank'>https://quinn0823.github.io</a> for Quinn-Web release version.",
+    "Visit <a href='https://github.com/quinn0823/web_preview' target='_blank'>https://github.com/quinn0823/web_preview</a> for the repository of Quinn-Web Preview."
+];
 
 var headernav_list = "<li><a href=''><img src='images/icons/large/icon-128.png' alt=''></a></li>";
 for (let i in pages) {
@@ -96,3 +96,15 @@ footer_directory = "<nav class='footer-directory'>" + footer_directory + "</nav>
 var footer_footer = "<section class='footer-footer'><div class='footer-footer-contact'>Email me at <a href='mailto:quinnqiu0823@icloud.com'>quinnqiu0823@icloud.com</a>.</div><div class='footer-footer-locale'>United States</div><div class='footer-footer-copyright'>Copyright © 2024 Quinn Qiu All Rights Reserved.</div></section>";
 
 document.getElementById("footer-content").innerHTML = document.getElementById("footer-content").innerHTML + footer_directory + footer_footer;
+
+if(footer_note_items != "") {
+    if (!document.getElementById("footer-note")) {
+        document.getElementById("footer-content").innerHTML = "<section id='footer-note'></section>" + document.getElementById("footer-content").innerHTML;
+    }
+    let footer_note = "";
+    for (let i = 0; i < footer_note_items.length; i++) {
+        footer_note = footer_note + "<li>" + footer_note_items[i] + "</li>"
+    }
+    footer_note = "<ul>" + footer_note + "</ul>";
+    document.getElementById("footer-note").innerHTML = document.getElementById("footer-note").innerHTML + footer_note;
+}
